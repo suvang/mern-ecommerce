4f342ef2946387ae7f22aa4abbe5e2e876ee89ce
suvang <suvang08@gmail.com>
2020-10-07 23:11:28 +0530

    prepared

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+/*module.exports = {
+    mongoURI:process.env.MONGO_URI
+} */
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://suvang:suvang123@ecommerce.wflea.mongodb.net/test?retryWrites=true&w=majority",
+};
